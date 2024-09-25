42c4a9a1993d23d2b6e1bdbd070738ee94d7c4f5
alpimp <alannapimple306@gmail.com>
2024-09-25 22:09:11 +0300

    Added html string

--- functions/submit.ts
+++ functions/submit.ts
@@ -22,11 +22,11 @@ feedback_msg
 
 const html = form1 + get_ip + form2 +  alpha_val + form2 +  demo_val + form2 +  display + form2 +  touch + form2 +  lang + form2 +  tz + form2 +  feedback_msg + form3
 
-const form1 = (<\!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title></title> </head> <body> <div> <p> <a href="">);
+const form1 = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title></title> </head> <body> <div> <p> <a href="">';
 
-const form2 = (</a> </p> </div> <div> <p> <a href="">); 
+const form2 = '</a> </p> </div> <div> <p> <a href="">'; 
 
-const form3 = (</a> </p> </div> </body> </html>);
+const form3 = '</a> </p> </div> </body> </html>';
 
          return new Response(html, {
             headers: {
