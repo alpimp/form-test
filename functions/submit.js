149aee96a678f5e0d8b2d827e12fbed88acaa5d1
alpimp <alannapimple306@gmail.com>
2024-09-25 23:25:25 +0300

    Added html string

--- functions/submit.js
+++ functions/submit.js
@@ -1,18 +1,24 @@
-export function onRequest(context) {
-//  return new Response("Hello, world!")
-    try {
-      const body = context.formData();
+export async function onRequestPost(context) {
+  try {
+    return await handleRequest(context);
+  } catch (e) {
+    console.error(e);
+    return new Response("Error sending message", { status: 500 });
+  }
+}
 
-      const { 
-        get_ip,
-        alpha_val,
-        demo_val,
-        display,
-        touch,
-        lang,
-        tz,
-        feedback_msg
-      } = Object.fromEntries(body);
+async function handleRequest({ request }) {
+  const ip = request.headers.get("CF-Connecting-IP");
+
+  const formData = await request.formData();
+  const get_ip = formData.get("get_ip");
+  const alpha_val = formData.get("alpha_val");
+  const demo_val = formData.get("demo_val");
+  const display = formData.get("display");
+  const touch = formData.get("touch");
+  const lang = formData.get("lang");
+  const tz = formData.get("tz");
+  const feedback_msg = formData.get("feedback_msg");
 
       const form1 = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title></title> </head> <body> <div> <p> <a href="">';
       const form2 = '</a> </p> </div> <div> <p> <a href="">'; 
@@ -20,19 +26,14 @@ export function onRequest(context) {
 
       const html = form1 + get_ip + form2 + alpha_val + form2 + demo_val + form2 + display + form2 + touch + form2 + lang + form2 + tz + form2 + feedback_msg + form3;
 
+
       return new Response(html, {
         headers: {
           'content-type': 'text/html',
         }
       });
+}
 
-    } catch (e) {
-      let pathname = new URL(request.url).pathname;
-      return new Response(`"${pathname}" not found`, {
-        status: 404,
-        statusText: 'not found',
-      });
-    }
-};
+
 
 
